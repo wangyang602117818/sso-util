13ccaaf776e9276259f585b4da580711a103483f
wangyang <18518317472@163.com>
2020-10-25 16:58:09 +0800

    add toast

--- plugins/index.js
+++ plugins/index.js
@@ -1,9 +1,19 @@
+import Vue from 'vue'
 import loading from '../icon/vue-icon-loading.vue'
 import vueserverselect from "./vue-server-select"
+import Toast from './toast';
+
+const ToastConstructor = Vue.extend(Toast);
+const toast = function(options = {}) {
+    const toastInstance = new ToastConstructor({data: options}).$mount();
+    document.body.appendChild(toastInstance.$el);
+    return toastInstance;
+};
+
 let Index = {}
 Index.install = function (Vue, options) {
-
+    Vue.prototype.$toast = toast;  //toast
     Vue.component(vueserverselect.name,vueserverselect)  //select
-    Vue.component(loading.name, loading)
+    Vue.component(loading.name, loading) //loading
 }
 export default Index
