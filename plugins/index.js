13ba28b896240d437ce9791f12291d59e2a0a09e
wangyang <18518317472@163.com>
2020-10-29 09:23:53 +0800

    bug

--- plugins/index.js
+++ plugins/index.js
@@ -4,8 +4,8 @@ import vueserverselect from "./vue-server-select"
 import Toast from './toast';
 
 const ToastConstructor = Vue.extend(Toast);
-const toast = function(options = {}) {
-    const toastInstance = new ToastConstructor({data: options}).$mount();
+const toast = function (type, message, duration = 1) {
+    const toastInstance = new ToastConstructor({ data: { type: type, message: message, duration: duration, show: true } }).$mount();
     document.body.appendChild(toastInstance.$el);
     return toastInstance;
 };
@@ -13,7 +13,7 @@ const toast = function(options = {}) {
 let Index = {}
 Index.install = function (Vue, options) {
     Vue.prototype.$toast = toast;  //toast
-    Vue.component(vueserverselect.name,vueserverselect)  //select
+    Vue.component(vueserverselect.name, vueserverselect)  //select
     Vue.component(loading.name, loading) //loading
 }
 export default Index
